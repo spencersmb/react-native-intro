47c9605f0323382a2e379cb2963f5e17bd1e4b91
spencer <spencer.bigum@gmail.com>
2018-01-19 03:44:51 -0500

    first commit with FLOW setup

---
+++ index.js
@@ -0,0 +1,8 @@
+import { AppRegistry } from 'react-native'
+import React from 'react'
+import App from './App'
+
+// Render to the device
+// must register at least one component to the application
+// the string must match the name of the project
+AppRegistry.registerComponent('intro', () => App)
---
+++ libdefs.js
@@ -0,0 +1,6 @@
+declare module 'react-native' {
+  declare export var Platform: any
+  declare export var StyleSheet: any
+  declare export var Text: any
+  declare export var View: any
+}
